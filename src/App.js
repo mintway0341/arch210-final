b40d6a8dccdfbd8e149a77b245461fb76510be4e
Yoomin Kang <mintway0341@gmail.com>
2022-12-08 15:52:19 +0900

    add images

--- src/App.js
+++ src/App.js
@@ -12,6 +12,20 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense } from "react";
 import { DDSLoader } from "three-stdlib";
 import { TypeAnimation } from "react-type-animation";
+import view1 from "./assets/3D-View-1.png";
+import view2 from "./assets/3D-View-2.png";
+import view3 from "./assets/3D-View-3.png";
+import elevationS from "./assets/Elevation-S.png";
+import elevationW from "./assets/Elevation-W.png";
+import circulationDiagram from "./assets/Circulation-Diagram.png";
+import conceptDiagram from "./assets/Concept-Diagram.png";
+import explodedDiagram from "./assets/Exploded-Diagram.png";
+import programDiagram from "./assets/Program-Diagram.png";
+import floorPlan1 from "./assets/1st-Floor-Plan.png";
+import floorPlan2 from "./assets/2nd-Floor-Plan.png";
+import sitePlan from "./assets/Site-Plan.png";
+import sectionA from "./assets/Section-A.png";
+import sectionB from "./assets/Section-B.png";
 
 THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
@@ -22,9 +36,9 @@ const Scene = () => {
   return (
     <primitive
       object={fbx}
-      scale={0.007}
-      rotation={[30.7, 197.8, 0.6]}
-      position={[-80, -190, -60]}
+      scale={0.01}
+      rotation={[30, 197.9, 0.6]}
+      position={[-100, -210, -140]}
     />
   );
 };
@@ -34,10 +48,10 @@ const App = () => {
     <div
       style={{
         backgroundColor: "black",
-        height: "100vh",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
+        paddingBottom: "100px",
       }}
     >
       {/* <p
@@ -53,25 +67,30 @@ const App = () => {
         repeat={3}
         style={{
           fontSize: "5vh",
-          marginTop: "20vh",
+          marginTop: "10vh",
           marginBottom: "0vh",
           fontWeight: 700,
           background: "-webkit-linear-gradient(300deg, #90c0a0, #70c0f0)",
           WebkitBackgroundClip: "text",
           WebkitTextFillColor: "transparent",
+          transform: "rotate(-5deg)",
         }}
       />
       <Canvas
-        orthographic={true}
+        orthographic={false}
         style={{ width: "100vw", height: "50vh" }}
         camera={{
           aspect: window.innerWidth / window.innerHeight,
           fov: 100,
-          near: 0.1,
+          near: 0.05,
           far: 1000,
         }}
       >
-        <OrbitControls />
+        <OrbitControls
+          autoRotate={true}
+          autoRotateSpeed={0.1}
+          maxPolarAngle={0.7}
+        />
         <directionalLight
           position={[10, 30, 20]}
           intensity={2}
@@ -84,6 +103,23 @@ const App = () => {
         />
         <Scene />
       </Canvas>
+      <img src={view1} style={{ width: "90vw", marginTop: "5vh" }} />
+      <img src={view2} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={view3} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={elevationS} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={elevationW} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={conceptDiagram} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img
+        src={circulationDiagram}
+        style={{ width: "90vw", marginTop: "10vh" }}
+      />
+      <img src={programDiagram} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={explodedDiagram} style={{ width: "90vw", marginTop: "0vh" }} />
+      <img src={floorPlan1} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={floorPlan2} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={sitePlan} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={sectionA} style={{ width: "90vw", marginTop: "10vh" }} />
+      <img src={sectionB} style={{ width: "90vw", marginTop: "10vh" }} />
     </div>
   );
 };
